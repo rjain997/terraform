76a642e531bcf8ef143c385fbb9dca2f5a81c9ef
RJ <wizardofdevops@gmail.com>
2025-05-26 16:33:15 +0000

    main.tf added

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}  # This is required by the azurerm provider
+  subscription_id = var.subscription_id
+}
+
+variable "subscription_id" {
+  description = "The Azure subscription ID"
+  type        = string
+  sensitive   = true
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "samurai"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
